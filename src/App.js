07e028e96e53d2808937b0f9dc16315194cf2270
Victor Gonzalez <dazedbutamused@gmail.com>
2019-11-20 21:20:36 -0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { Route, Switch, Redirect, Link } from "react-router-dom";
-import userService from './utils/userService'
+import userService from "./utils/userService";
 import SignupPage from "./pages/SignUpPage/SignUpPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import NavBar from './components/NavBar/NavBar';
-import GameBoard from './components/GameBoard/GameBoard';
-
-
+import NavBar from "./components/NavBar/NavBar";
+import GameBoard from "./components/GameBoard/GameBoard";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       user: userService.getUser()
-    }
+    };
   }
   handleLogout = () => {
     userService.logout();
@@ -26,45 +24,40 @@ class App extends Component {
     this.setState({ user: userService.getUser() });
   };
 
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-      <main>
-      <Switch>
-        <Route exact path ="/" render={({history}) => (
-
-          <GameBoard />
-        )}
-        />
-      <Route
-            exact
-            path="/signup"
-            render={({ history }) => (
-              <SignupPage
-                history={history}
-                handleSignupOrLogin={this.handleSignupOrLogin}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/login"
-            render={({ history }) => (
-              <LoginPage
-                history={history}
-                handleSignupOrLogin={this.handleSignupOrLogin}
-              />
-            )}
-          />
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" render={({ history }) => <GameBoard />} />
+            <Route
+              exact
+              path="/signup"
+              render={({ history }) => (
+                <SignupPage
+                  history={history}
+                  handleSignupOrLogin={this.handleSignupOrLogin}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/login"
+              render={({ history }) => (
+                <LoginPage
+                  history={history}
+                  handleSignupOrLogin={this.handleSignupOrLogin}
+                />
+              )}
+            />
           </Switch>
-
-      </main>
-    </div>
-  );
-            }
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
