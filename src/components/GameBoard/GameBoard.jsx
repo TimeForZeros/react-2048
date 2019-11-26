0c54f337014c101d2c68a28cb221b9ba4077a330
Victor Gonzalez <dazedbutamused@gmail.com>
2019-11-26 15:30:24 -0800

    Trying to refactor gameboard

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -18,217 +18,39 @@ const colors = {
   "2048": "#962425"
 };
 
-let board = [
-  [0, 0, 0, 0], //Row 1 idx 0
-  [0, 0, 0, 0], //Row 2 idx 1
-  [0, 0, 0, 0], //Row 3 idx 2
-  [0, 0, 0, 0] //Row 4 idx 3
-];
+// let board = [
+//   [0, 0, 0, 0], //Row 1 idx 0
+//   [0, 0, 0, 0], //Row 2 idx 1
+//   [0, 0, 0, 0], //Row 3 idx 2
+//   [0, 0, 0, 0] //Row 4 idx 3
+// ];
 
-const boardRender = (arr) => {
-    let newArr = []
-    //function that goes through the board array rows
-    arr.forEach(function(colArr, rowIdx) {
-      //function that goes through the column index of the row arrays
-      colArr.forEach(function(cell, colIdx) {
-        newArr.push(React.createElement("div", {
-            id: `c${colIdx}r${rowIdx}`,
-            style: {
-              display: 'block',  
-              width: "90px",
-              height: "90px",
-              backgroundColor: `${colors[cell]}`,
-              margin: "5px",
-              borderRadius: '20px'
-            }
-          }))
-      });
-    });
-    return newArr;
-  };
 
 function randomNum() {
-  return Math.floor(Math.random() * 4);
+ return Math.floor(Math.random() * 4);
 }
-
-  function renderUpdate() {
-    let col = randomNum();
-    let row = randomNum();
-    let updateNum = null;
-    let tileUpdate = document.getElementById(`c${col}r${row}`);
-  
-    if (board[row][col] === 0) {
-
-      // board[row][col];
-      if (randomNum() > 2) {
-        board[row][col] = 4;
-      } else {
-        board[row][col] = 2;
-      }
-    } else {
-      renderUpdate(board);
-    }
-    updateNum = board[row][col];
-    return (tileUpdate.style.backgroundColor = colors[updateNum.toString()]);
-  }
 
 
 
 //randomly initializes two elements with values of 2
-function spawnInit(arr) {
-  for (var i = 0; i < 2; ) {
-    let col = randomNum();
-    let row = randomNum();
-    let updateNum = null;
-    let tileUpdate = document.getElementById(`c${col}r${row}`);
-    if (board[row][col] === 0) {
-      board[row][col] = 2;
-      updateNum = board[row][col];
-      i++;
-      // tileUpdate.style.backgroundColor = colors[updateNum.toString()];
-    }
-  }
-  return arr;
-}
-
-  const moves = {
-    left: function shiftLeft(boardArr) {
-      boardArr.forEach(function(arr, idx) {
-        boardArr[idx] = leftMove(arr);
-        return boardArr[idx];
-      });
-      return boardArr;
-    },
-    right: function shiftRight(boardArr) {
-      boardArr.forEach(function(arr, idx) {
-        boardArr[idx] = rightMove(arr);
-        return boardArr[idx];
-      });
-      return boardArr;
-    },
-    up: function shiftUp(boardArr) {
-      boardArr = colConverter(boardArr);
-      boardArr.forEach(function(arr, idx) {
-        boardArr[idx] = leftMove(arr);
-        return boardArr[idx];
-      });
-      boardArr = colConverter(boardArr);
-      return boardArr;
-    },
-    down: function shiftDown(boardArr) {
-      boardArr = colConverter(boardArr);
-      boardArr.forEach(function(arr, idx) {
-        boardArr[idx] = rightMove(arr);
-        return boardArr[idx];
-      });
-      boardArr = colConverter(boardArr);
-      return boardArr;
-    }
-  };
-
-    document.onkeydown = function(e) {
-    switch (e.keyCode) {
-      case 37: //left
-        {
-          const sentryArray = JSON.stringify(board);
-          let sentryCheck = "";
-          let tempArray = boardRender(moves.left(board));
-          winCheck(tempArray);
-          sentryCheck = JSON.stringify(board);
-          board = Array.from(tempArray);
-          if (sentryArray !== sentryCheck) {
-            loseCheck(board);
-            renderUpdate(board);
-          } else return;
-        }
-        break;
-      case 38: //up
-        {
-          const sentryArray = JSON.stringify(board);
-          let sentryCheck = "";
-          let tempArray = boardRender(moves.up(board));
-          winCheck(tempArray);
-          sentryCheck = JSON.stringify(tempArray);
-          board = Array.from(tempArray);
-          if (sentryArray !== sentryCheck) {
-            loseCheck(board);
-            renderUpdate(board);
-          } else return;
-        }
-        break;
-      case 39: //right
-        {
-          const sentryArray = JSON.stringify(board);
-          let sentryCheck = "";
-          let tempArray = boardRender(moves.right(board));
-          // winCheck(tempArray);
-          sentryCheck = JSON.stringify(tempArray);
-          board = Array.from(tempArray);
-          if (sentryArray !== sentryCheck) {
-            loseCheck(board);
-            renderUpdate(board);
-          } else return;
-        }
-        break;
-      case 40: //down
-        {
-          const sentryArray = JSON.stringify(board);
-          let sentryCheck = "";
-          let tempArray = boardRender(moves.down(board));
-          // winCheck(tempArray);
-          sentryCheck = JSON.stringify(tempArray);
-          board = Array.from(tempArray);
-          if (sentryArray !== sentryCheck) {
-            loseCheck(board);
-            renderUpdate(board);
-          } else return;
-        }
-        break;
-    }
-  };
-  
-  //check for win
-  function winCheck(arr) {
-    arr.forEach(function(rowArr) {
-      rowArr.forEach(function(winNumber) {
-        if (winNumber === 2048) {
-          alert("YOU WIN!!!!");
-        }
-      });
-    });
-  }
-  
-  //check for lose
-  function loseCheck(arr) {
-    return;
-    arr.forEach(function(rowArr) {
-      rowArr.forEach(function(isZero) {
-        while (isZero != 0)
-          if (isZero === 0) {
-            return;
-          } else {
-            let checkBoard = Array.from(board);
-            let checkUp = JSON.stringify(moves.up(checkBoard));
-            let checkDown = JSON.stringify(moves.down(checkBoard));
-            let checkLeft = JSON.stringify(moves.left(checkBoard));
-            let checkRight = JSON.stringify(moves.right(checkBoard));
-            if (checkUp === checkDown && checkLeft === checkRight) {
-              alert("You lose!");
-            } else {
-              return;
-            }
-          }
-      });
-    });
-  }
-  //score counter
 
 
-function init() {
-    // scoreDisplay.textContent = 0;
-    boardRender(board);
-    spawnInit(board);
-  }
+//BACKUP
+// function spawnInit(arr) {
+//   for (var i = 0; i < 2; ) {
+//     let col = randomNum();
+//     let row = randomNum();
+//     let updateNum = null;
+//     let tileUpdate = document.getElementById(`c${col}r${row}`);
+//     if (this.state.board[row][col] === 0) {
+//       this.state.board[row][col] = 2;
+//       updateNum = this.state.board[row][col];
+//       i++;
+//       // tileUpdate.style.backgroundColor = colors[updateNum.toString()];
+//     }
+//   }
+//   return arr;
+// }
 
 
 
@@ -298,15 +120,79 @@ function init() {
   }
 
 
-
 class GameBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board:  [ 
+      [0, 0, 0, 0], //Row 1 idx 0
+      [0, 0, 0, 0], //Row 2 idx 1
+      [0, 0, 0, 0], //Row 3 idx 2
+      [0, 0, 0, 0] //Row 4 idx 3
+      ],
+    }
+  }
+
+
+  boardRender = (arr) => {
+    let newArr = [];
+    //function that goes through the board array rows
+    arr.forEach(function(colArr, rowIdx) {
+      //function that goes through the column index of the row arrays
+      colArr.forEach(function(cell, colIdx) {
+        newArr.push(React.createElement("div", {
+            id: `c${colIdx}r${rowIdx}`,
+            style: {
+              display: 'block',  
+              width: "90px",
+              height: "90px",
+              backgroundColor: `${colors[cell]}`,
+              margin: "5px",
+              borderRadius: '20px'
+            },
+            value: 0
+          }))
+      });
+    });
+    return newArr;
+  };
+
+  spawnInit(arr) {
+    for (var i = 0; i < 2; ) {
+      let col = randomNum();
+      let row = randomNum();
+      let updateNum = null;
+      let board = arr.slice();
+      let tileUpdate = document.getElementById(`c${col}r${row}`);
+      if (board[row][col] === 0) {
+        board[row][col] = 2;
+        updateNum = board[row][col];
+        this.setState({board: board});
+        i++;
+        // tileUpdate.style.backgroundColor = colors[updateNum.toString()];
+  
+      }
+    }
+    return arr;
+  }
+
+  init() {
+    // scoreDisplay.textContent = 0;
+    this.boardRender(this.state.board);
+    this.spawnInit(this.state.board);
+  }
+
+
+
+
+
   render() {
-    init();
+    this.init();
     return (
       <div>
         <div className="main">
           <h2>2048 </h2>
-          <section className='grid'> {boardRender(board)}</section>
+          <section className='grid'> {this.boardRender(this.state.board)}</section>
         </div>
       </div>
     );
--- src/components/GameBoard/Moves.jsx
+++ src/components/GameBoard/Moves.jsx
@@ -1,8 +1,38 @@
-let board = [
-    [0, 0, 0, 0], //Row 1 idx 0
-    [0, 0, 0, 0], //Row 2 idx 1
-    [0, 0, 0, 0], //Row 3 idx 2
-    [0, 0, 0, 0] //Row 4 idx 3
-  ];
+
+  const moves = {
+    left: function shiftLeft(boardArr) {
+      boardArr.forEach(function(arr, idx) {
+        boardArr[idx] = leftMove(arr);
+        return boardArr[idx];
+      });
+      return boardArr;
+    },
+    right: function shiftRight(boardArr) {
+      boardArr.forEach(function(arr, idx) {
+        boardArr[idx] = rightMove(arr);
+        return boardArr[idx];
+      });
+      return boardArr;
+    },
+    up: function shiftUp(boardArr) {
+      boardArr = colConverter(boardArr);
+      boardArr.forEach(function(arr, idx) {
+        boardArr[idx] = leftMove(arr);
+        return boardArr[idx];
+      });
+      boardArr = colConverter(boardArr);
+      return boardArr;
+    },
+    down: function shiftDown(boardArr) {
+      boardArr = colConverter(boardArr);
+      boardArr.forEach(function(arr, idx) {
+        boardArr[idx] = rightMove(arr);
+        return boardArr[idx];
+      });
+      boardArr = colConverter(boardArr);
+      return boardArr;
+    }
+  };
+
 
   export default board
