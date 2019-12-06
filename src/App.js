3ce4439f3e1407bafa04edd74a912609a3dc9ff8
Victor Gonzalez <dazedbutamused@gmail.com>
2019-12-06 12:26:32 -0800

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -13,17 +13,10 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: userService.getUser()
+      score: null,
     };
   }
-  handleLogout = () => {
-    userService.logout();
-    this.setState({ user: null });
-  };
 
-  handleSignupOrLogin = () => {
-    this.setState({ user: userService.getUser() });
-  };
 
   render() {
     return (
@@ -32,29 +25,7 @@ class App extends Component {
           <NavBar />
         </header>
         <main className="container">
-          <Switch>
             <Route exact path="/" render={({ history }) => <GameBoard />} />
-            <Route
-              exact
-              path="/signup"
-              render={({ history }) => (
-                <SignupPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
-                />
-              )}
-            />
-            <Route
-              exact
-              path="/login"
-              render={({ history }) => (
-                <LoginPage
-                  history={history}
-                  handleSignupOrLogin={this.handleSignupOrLogin}
-                />
-              )}
-            />
-          </Switch>
         </main>
       </div>
     );
