e707ca8f191840fe43e347090077da74ffa01ecb
Victor Gonzalez <dazedbutamused@gmail.com>
2020-06-07 12:49:12 -0700

    changes

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 
-//use below if not working
+// use below if not working
 // mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const favicon = require("serve-favicon");
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
 
 require('dotenv').config();
 require('./config/database');
@@ -10,25 +10,22 @@ const scoreRouter = require('./routes/api/scores');
 
 const app = express();
 
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
-app.use(logger("dev"));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
-
-
-//APIs
+// APIs
 app.use('/api/score', scoreRouter);
 //  app.use('/api/users', require('./routes/api/users'));
 // app.use('/api/events', require('./routes/api/events'));
 
-
-app.get('/*', function(req, res) {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-    console.log(`Express app running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`);
 });
